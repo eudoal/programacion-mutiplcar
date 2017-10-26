a9d6e11b45e39593db0da3f43bb307924be82f2d
Halleck_Lenovo\Prueba <halleck.g.florida@gmail.com>
2017-10-26 22:33:15 +0200

    Modificado

--- JS/multiplicador10.js
+++ JS/multiplicador10.js
@@ -1,28 +1,23 @@
-//do {
-// pide un numero
 
-  var numero=prompt("Introduce un numero entre el 1 y el 100");
-  var i=0;
-    if ((numero > 100) || (numero <1))
-    {
-//        alert(numero+ " no es un número del 1 al 100");
-          console.log(numero+ " no es un número del 1 al 100");
-      }
+//  var numero=prompt("Introduce un numero entre el 1 y el 100");
 
-     else if (isNaN(numero))
-      {
-//              alert(numero+" no es un número");
-                console.log(numero+" no es un número");
-        }
 
-      else if ((numero < 100) || (numero >1))
-      {
-        do {
-          i++;
-//          alert ((numero+ " X "+ i+ " = "+ numero*i+ "\n"));
-          console.log((numero+ " X "+ i+ " = "+ numero*i+ "\n"));
-          }
-           while (i < 10);
-          }
+do {
 
-  //        } while ((numero < 100) || (numero >1));
+  var respuesta=prompt("Introduce un numero entre el 1 y el 100");
+  var numero=parseInt(respuesta);
+  if ((numero > 100) || (numero <1))
+  {
+    console.log(numero+ " no es un número del 1 al 100");
+  }
+  else if (isNaN(numero))
+  {
+    console.log(numero+" no es un número");
+  }
+
+} while ((isNaN (numero))||(numero > 100)|| (numero <1));
+var i=0;
+  do {
+    i++;
+    console.log((numero+ " X "+ i+ " = "+ numero*i+ "\n"));
+    }while (i < 10);
